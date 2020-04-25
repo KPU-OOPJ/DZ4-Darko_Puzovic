45045c1f33d6f632460abd5bd0002b244f156816
darkoP98 <darko.puzovic98@gmail.com>
2020-04-25 18:00:15 +0200

    2b1-0014-17

---
+++ src/konvertor.java
@@ -0,0 +1,21 @@
+
+public class konvertor {
+    public double konvertujKmUMi (double kilometri){
+        return kilometri/1.67;
+    }
+    public double konvertujMiuKm (double milje){
+        return milje*1.67;
+    }
+    public double konvertujMUSt (double metri){
+        return metri/0.32;
+    }
+    public double konvertujStUM (double stope){
+        return stope*0.32;
+    }
+    public double konvertujKgULb (double kilogrami){
+        return kilogrami/0.45;
+    }
+    public double konvertujLbUKg (double libre){
+        return libre*0.45;
+    }
+}
